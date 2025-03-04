6af9ba3cc29f66d3648805f89d41eae3abee6543
Hanna Melnychuk <annamelnichuk2512@gmail.com>
2025-03-03 17:22:36 -0700

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,16 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const totalCost = days * 40;
+
   if (days >= 7) {
-    return days * 40 - 50;
+    return totalCost - 50;
   }
 
   if (days >= 3) {
-    return days * 40 - 20;
+    return totalCost - 20;
   }
 
   if (days < 3) {
-    return days * 40;
+    return totalCost;
   }
 }
 
